ae0e7cd2d6f7904efc2c6f941018430870c30a50
Conor O'Neill <conor8147@gmail.com>
2020-12-05 13:38:49 +1300

    init backend

---
+++ backend/src/test/kotlin/com/conor/notion/NotIonApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.conor.notion
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotIonApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
